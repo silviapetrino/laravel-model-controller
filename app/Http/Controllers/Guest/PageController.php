b07160e800a14890c5df8406f0feb7a533a91ecb
silviapetrino <petrino.silvia@gmail.com>
2023-11-20 14:55:09 +0100

    [fix]: route

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -10,7 +10,6 @@ class PageController extends Controller
 {
     public function index(){
 
-
         return view('home');
 
     }
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PageController::class, 'index'])->name('home');
+Route::get('/', 'App\Http\Controllers\Guest\PageController@index')->name('home');
